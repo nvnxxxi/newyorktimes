4f1545026125c70dc5027a760593cf69ecf67c01
Cristian Colocho <chosafine@icloud.com>
2019-12-11 13:58:33 -0500

    updating logic

--- script.js
+++ script.js
@@ -3,27 +3,25 @@ $("button").click(function() {
   const query = $(this).val();
   const fromDate = $("#from-date").value;
   const toDate = $("#to-date").value;
-  const queryURL =
-    `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&facet=true&begin_date=${fromDate}&end_date=${toDate}&api-key=CBxn9s26XG0ivGlGxRAWeoP4FZfWAzAD`;
+  const queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&facet=true&begin_date=${fromDate}&end_date=${toDate}&api-key=CBxn9s26XG0ivGlGxRAWeoP4FZfWAzAD`;
 
   // ajax call to hit API and pull data to display
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then( (response) => {
+  }).then((response) => {
     for (element in results) {
-    
       // bind response to object body
-      const results = response.docs;
-      
+      const results = response.docs[element];
+
       // initialize layout for dynamically generated elements
       const resultsDiv = $("<div>");
-      const h1 = $("<h1>")
-      const p = $("<p>")
+      const h1 = $("<h1>");
+      const p = $("<p>");
 
       // write content to elements
-      h1.innerHTML = `<a href="${results.multimedia.url}">${results.headline.main}</a>`
-      p.text = `${results.pub_date} \n ${results.snippet}`
+      h1.innerHTML = `<a href="${results.multimedia.url}">${results.headline.main}</a>`;
+      p.text = `${results.pub_date} \n ${results.snippet}`;
 
       // write content to page
       resultsDiv.append(h1);
